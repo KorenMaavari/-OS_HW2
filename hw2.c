f7e095781f46820ac5b0625f0449abdd524f5079
Koren Ma'avari <koren130@gmail.com>
2024-07-02 22:42:14 +0000

    get_path_sum

--- hw2.c
+++ hw2.c
@@ -18,3 +18,27 @@ asmlinkage long sys_set_weight(int weight) {
 asmlinkage long sys_get_weight(void) {
     return current->weight;
 }
+
+asmlinkage long sys_get_path_sum(pid_t target) {
+    struct task_struct *task;
+    int sum = 0;
+
+    task = pid_task(find_vpid(target), PIDTYPE_PID); // Find the task struct for the given PID
+    if (!task) {
+        return -ECHILD;
+    }
+
+    while (task) {
+        sum += task->weight;
+        if (task == current) {
+            break;
+        }
+        task = task->parent;
+    }
+
+    if (task != current) {
+        return -ECHILD;
+    }
+
+    return sum;
+}
